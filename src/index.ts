5e6d961965c661220ead96d6f9da655f1ee7336f
Joshua Sosso <josh@modiimedia.com>
2023-08-31 13:54:17 -0500

    add typeSuffix config option

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import { generatorHandler } from '@prisma/generator-helper';
 import { createTransformer } from './generator/transformDMMF';
+import { parseEnvValue } from '@prisma/internals';
 import * as fs from 'fs';
 import * as path from 'path';
 import prettier from 'prettier';
+
+interface GeneratorConfig {
+  typeSuffix?: string;
+}
 
 generatorHandler({
   onManifest() {
@@ -12,33 +17,56 @@ generatorHandler({
     };
   },
   async onGenerate(options) {
-    const transformDMMF = createTransformer(options.generator.name);
+    const generatorConfig = options.generator.config as GeneratorConfig;
+    const transformDMMF = createTransformer(
+      options.generator.name,
+      generatorConfig.typeSuffix ?? '',
+    );
     const payload = transformDMMF(options.dmmf);
-    if (options.generator.output) {
-      const outputDir =
-        // This ensures previous version of prisma are still supported
-        typeof options.generator.output === 'string'
-          ? (options.generator.output as unknown as string)
-          : // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            '';
-      if (!outputDir) {
-        throw new Error('Must specify output for prisma-typebox-generator');
-      }
-      try {
-        await fs.promises.mkdir(outputDir, {
-          recursive: true,
-        });
-        const barrelFile = path.join(outputDir, 'index.ts');
-        await fs.promises.writeFile(barrelFile, '', {
-          encoding: 'utf-8',
-        });
-        await Promise.all(
-          payload.map(async (n) => {
-            const fsPromises = [];
+    if (!options.generator.output) {
+      throw new Error('No output was specified for Prisma Typebox Generator');
+    }
+    const outputDir =
+      // This ensures previous version of prisma are still supported
+      typeof options.generator.output === 'string'
+        ? (options.generator.output as unknown as string)
+        : // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          parseEnvValue(options.generator.output);
+    try {
+      await fs.promises.mkdir(outputDir, {
+        recursive: true,
+      });
+      const barrelFile = path.join(outputDir, 'index.ts');
+      await fs.promises.writeFile(barrelFile, '', {
+        encoding: 'utf-8',
+      });
+      await Promise.all(
+        payload.map(async (n) => {
+          const fsPromises = [];
+          fsPromises.push(
+            fs.promises.writeFile(
+              path.join(outputDir, n.name + '.ts'),
+              await prettier.format(n.rawString, {
+                parser: 'babel-ts',
+              }),
+              {
+                encoding: 'utf-8',
+              },
+            ),
+          );
+
+          fsPromises.push(
+            fs.promises.appendFile(
+              barrelFile,
+              `export * from './${n.name}';\n`,
+              { encoding: 'utf-8' },
+            ),
+          );
+          if (n.inputRawString) {
             fsPromises.push(
               fs.promises.writeFile(
-                path.join(outputDir, n.name + '.ts'),
-                await prettier.format(n.rawString, {
+                path.join(outputDir, n.name + 'Input.ts'),
+                await prettier.format(n.inputRawString, {
                   parser: 'babel-ts',
                 }),
                 {
@@ -46,46 +74,23 @@ generatorHandler({
                 },
               ),
             );
-
             fsPromises.push(
               fs.promises.appendFile(
                 barrelFile,
-                `export * from './${n.name}';\n`,
+                `export * from './${n.name}Input';\n`,
                 { encoding: 'utf-8' },
               ),
             );
-            if (n.inputRawString) {
-              fsPromises.push(
-                fs.promises.writeFile(
-                  path.join(outputDir, n.name + 'Input.ts'),
-                  await prettier.format(n.inputRawString, {
-                    parser: 'babel-ts',
-                  }),
-                  {
-                    encoding: 'utf-8',
-                  },
-                ),
-              );
-              fsPromises.push(
-                fs.promises.appendFile(
-                  barrelFile,
-                  `export * from './${n.name}Input';\n`,
-                  { encoding: 'utf-8' },
-                ),
-              );
-            }
+          }
 
-            return Promise.all(fsPromises);
-          }),
-        );
-      } catch (e) {
-        console.error(
-          'Error: unable to write files for Prisma Typebox Generator',
-        );
-        throw e;
-      }
-    } else {
-      throw new Error('No output was specified for Prisma Typebox Generator');
+          return Promise.all(fsPromises);
+        }),
+      );
+    } catch (e) {
+      console.error(
+        'Error: unable to write files for Prisma Typebox Generator',
+      );
+      throw e;
     }
   },
 });
